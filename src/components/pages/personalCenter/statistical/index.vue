afc67fcc7e05043ca0c31d4bcaacb8eeae876942
gjybjyozo <gjy@bjyozo.com>
2022-03-04 17:29:39 +0800

    1

--- src/components/pages/personalCenter/statistical/index.vue
+++ src/components/pages/personalCenter/statistical/index.vue
@@ -1,16 +1,49 @@
 <template>
-  <div>
-  statistical
-  </div>
+    <div>
+        statistical
+        {{pageSize}} --- {{pageNum}}
+        <indexTwo
+          :total="total"
+          @handleSizeOrNum="handleSizeOrNum"
+        ></indexTwo>
+        <button @click="one">改变total</button>
+          <!-- @handleCurrentChange="handleCurrentChange" -->
+          <!-- :pageSize="pageSize" -->
+          <!-- :pageNum="pageNum" -->
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Statistical',
-  components: {},
-}
+import indexTwo from './indexTwo.vue'
+export default {
+    name: "Statistical",
+    components: {
+      indexTwo
+    },
+    data() {
+        return {
+          pageSize: 20,
+          pageNum: 1,
+          total: 100
+        };
+    },
+    methods: {
+      one() {
+        this.total -1 
+        this.pageNum > 1 ? this.pageNum - 1 : 1
+
+        this.total = 40
+        
+      },
+      handleSizeOrNum(val, type) {
+        if (type === "pageSize") {
+          this.pageSize = val
+        }else {
+          this.pageNum = val
+        }
+      }
+    },
+};
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang="less" scoped></style>
---
+++ src/components/pages/personalCenter/statistical/indexTwo.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <el-pagination
+            :total="total"
+            :page-size="pageSize"
+            :page-sizes="pageSizes"
+            :current-page="pageNum"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            layout="total, sizes, prev, pager, next, jumper"
+        >
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "indexTwo",
+    props: {
+        pageSize: {
+            type: Number,
+            default: 20,
+        },
+        pageNum: {
+            type: Number,
+            default: 1,
+        },
+        total: {
+            type: Number,
+            default: 0,
+        },
+        pageSizes: {
+            type: Array,
+            default: () => {
+                return [10, 20, 40, 60];
+            },
+        },
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        handleSizeChange(val) {
+            console.log("handleSizeChange", val)
+            this.$emit("handleSizeOrNum", val, "pageSize");
+        },
+        handleCurrentChange(val) {
+            console.log("handleCurrentChange", val)
+
+            clearTimeout(this.currenChangeTime);
+            this.currenChangeTime = setTimeout(() => {
+                this.$emit("handleSizeOrNum", val, "pageNum");
+            }, 300);
+        },
+    },
+};
+</script>
+
+<style lang="less" scoped></style>
